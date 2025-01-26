500d861435bc4392e281f62c51bc8718e0825504
Mateusz Grzeliński <grzelinskimat@gmail.com>
2025-01-26 18:52:02 +0100

    Add support for loading MakroCollection.dat

---
+++ src/loadMakroCollection.go
@@ -0,0 +1,305 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"image/color"
+	"io"
+	"log"
+	"os"
+	"unicode/utf8"
+)
+
+type MakroName string
+type MakroPath string
+type MakroNameMapping map[MakroName]MakroPath
+type MakroCollection []*MakroCollectionItem
+
+type MakroCollectionItem struct {
+	FileName    MakroPath
+	Name        MakroName
+	Category    string
+	TextColorFG color.Color
+	TextColorBG color.Color
+}
+
+const (
+	KWMakroCollection      = "mkc"
+	KWItems                = "items"
+	KWMakroCollectionItem  = "mki"
+	KWMakroName            = "cap"
+	KWMakroCategory        = "cat"
+	KWMakroFileName        = "fn"
+	KWMakroBackgroundColor = "bc"
+	KWMakroForegroundColor = "fc"
+	KWUnknownUQ            = "UQ"
+)
+
+const (
+	HexDoNothing = 0x00 // Workaround, if the parsing was 100 correct this would not be needed
+	HexPadding   = 0x01 // not sure if this is correct
+	// marks unknown seqence of bytes with ascii "UQ"
+	HexUnknownUQ            = 0x02
+	HexUnknownUQLen         = 2
+	HexUnknownMaybeIndex    = 0x03
+	HexUnknownMaybeIndexLen = 2
+	HexString               = 0x06 // dynamic len
+	HexSection              = 0x07 // dynamic len
+	HexPadding4Byte         = 0x12
+	HexPadding4ByteLen      = 4
+	HexStringUtf            = 0x14
+)
+
+func LoadMakroCollection(path string) (MakroCollection, error) {
+	var result = MakroCollection{}
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	br := bufio.NewReader(f)
+	var lastItem *MakroCollectionItem = nil
+	for {
+		b, err := br.ReadByte()
+		if err != nil && !errors.Is(err, io.EOF) {
+			fmt.Println(err)
+			break
+		}
+		switch b {
+		case HexUnknownMaybeIndex:
+			if ReadSkip(br, HexUnknownMaybeIndexLen) != nil {
+				return nil, err
+			}
+		case HexSection:
+			sec, err := ReadLenAndSection(br)
+			if err != nil {
+				return nil, err
+			}
+			fmt.Print("Section: ")
+			fmt.Println(*sec)
+			switch *sec {
+			case KWMakroCollectionItem:
+				lastItem = new(MakroCollectionItem)
+				result = append(result, lastItem)
+			}
+		case HexString:
+			keyWord, err := ReadLenAndString(br)
+			if err != nil {
+				return nil, err
+			}
+			fmt.Print("Key: ")
+			fmt.Println(*keyWord)
+			lastValidKW := *keyWord
+			switch lastValidKW {
+			case KWMakroName:
+				value, err := ReadKWAndLenAndString(br)
+				if err != nil {
+					return nil, err
+				}
+				fmt.Printf("Value: %s\n", *value)
+				lastItem.Name = MakroName(*value)
+			case KWMakroCategory:
+				value, err := ReadKWAndLenAndString(br)
+				if err != nil {
+					return nil, err
+				}
+				lastItem.Category = *value
+			case KWMakroFileName:
+				value, err := ReadKWAndLenAndString(br)
+				if err != nil {
+					return nil, err
+				}
+				lastItem.FileName = MakroPath(*value)
+			case KWMakroForegroundColor:
+				color, err := ReadLenAndColor(br)
+				if err != nil {
+					return nil, err
+				}
+				lastItem.TextColorFG = color
+			case KWMakroBackgroundColor:
+				color, err := ReadLenAndColor(br)
+				if err != nil {
+					return nil, err
+				}
+				lastItem.TextColorBG = color
+			}
+		case HexDoNothing:
+		}
+		if err != nil {
+			// end of file
+			break
+		}
+	}
+	return result, nil
+}
+
+func ReadSkip(r io.Reader, n uint8) error {
+	_, err := io.ReadAll(io.LimitReader(r, int64(n)))
+	return err
+}
+
+// read exactly n bytes
+func ReadKWAndLenAndString(r *bufio.Reader) (*string, error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	if b == HexString {
+		return ReadLenAndString(r)
+	} else if b == HexPadding4Byte {
+		err := ReadSkip(r, HexPadding4ByteLen)
+		if err == nil {
+			temp := ""
+			return &temp, nil
+		} else {
+			return nil, err
+		}
+	} else if b == HexStringUtf {
+		return ReadLenAndUFT8String(r)
+	} else {
+		return nil, fmt.Errorf("byte does not indicate string: %d", b)
+	}
+}
+
+func ReadLenAndUFT8String(r *bufio.Reader) (*string, error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	n := int(uint8(b))
+
+	b, err = r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	if b != 0 {
+		log.Printf("warning when reading utf8 string: not zero")
+	}
+	b, err = r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	if b != 0 {
+		log.Printf("warning when reading utf8 string: not zero")
+	}
+	b, err = r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	if b != 0 {
+		log.Printf("warning when reading utf8 string: not zero")
+	}
+	var runes []rune = []rune{}
+	for len(runes) < n-2 {
+		// Read a single byte
+		b, err := r.ReadByte()
+		if err != nil {
+			return nil, fmt.Errorf("error reading file: %w", err)
+		}
+
+		// Peek more bytes to determine the complete rune
+		bytes := []byte{b}
+		for !utf8.FullRune(bytes) {
+			nextByte, err := r.ReadByte()
+			if err != nil {
+				return nil, fmt.Errorf("error reading file: %w", err)
+			}
+			bytes = append(bytes, nextByte)
+		}
+		r, size := utf8.DecodeRune(bytes)
+		if r == utf8.RuneError && size == 1 {
+			return nil, fmt.Errorf("invalid UTF-8 sequence encountered")
+		}
+		runes = append(runes, r)
+	}
+	tmp := string(runes)
+	return &tmp, nil
+}
+
+func ReadLenAndString(r *bufio.Reader) (*string, error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	n := uint8(b)
+	bytes, err := io.ReadAll(io.LimitReader(r, int64(n)))
+	if err != nil {
+		return nil, err
+	}
+	out := string(bytes)
+	return &out, nil
+}
+
+// read untill you find 0x00 but take only n bytes and discard the rest
+func ReadLenAndSection(r *bufio.Reader) (*string, error) {
+	bn, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	n := uint8(bn)
+	bytes, err := r.ReadBytes(0)
+	if err != nil {
+		return nil, err
+	}
+	// ignore bytes bigger than n (asssume it is padding)
+	out := string(bytes[:n])
+	return &out, nil
+}
+
+func ReadLenAndColor(r *bufio.Reader) (color.Color, error) {
+	colorLength, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	n := uint8(colorLength) // will be always 4: R,G,B,A
+	if n == 4 {
+		R, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		G, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		B, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		A, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		out := color.NRGBA{R: R, G: G, B: B, A: A}
+		return out, nil
+	} else if n == 3 {
+		// todo might be also HSV?
+		R, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		G, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		B, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		out := color.NRGBA{R: R, G: G, B: B, A: 0}
+		return out, nil
+	} else if n == 2 {
+		grayscale, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		A, err := r.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+		out := color.NRGBA{R: grayscale, G: grayscale, B: grayscale, A: A}
+		return out, nil
+	} else {
+		return nil, fmt.Errorf("unusual color length: %d", n)
+	}
+}
---
+++ src/loadMakroCollection_test.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"image/color"
+	"path/filepath"
+	"testing"
+)
+
+var pathToTestMakroCollection = filepath.Join("..", "tests", "makroCollection")
+var testFilesMakroCollection = []string{
+	"MakroCollectionMinimal.dat",
+	"MakroCollection2Items.dat",
+	"MakroCollection2ItemsWithSameName.dat",
+	"MakroCollectionCorpus5CompleteExampleStolarz.dat",
+}
+
+func TestLoadMakroCollectionMinimal(t *testing.T) {
+	path := filepath.Join(pathToTestMakroCollection, testFilesMakroCollection[0])
+	out, err := LoadMakroCollection(path)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	if len(out) != 1 {
+		t.Log(out)
+		t.Error("wrong len")
+		t.FailNow()
+	}
+	item := out[0]
+	if item.Name != "custom" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "Kategoria" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "custom.CMK" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+	colorFG := color.NRGBA{R: 0x01, G: 0x01, B: 0x01, A: 0x0}
+	if item.TextColorFG != colorFG {
+		t.Log(item.TextColorFG)
+		t.Errorf("wrong FG color: %s", item.TextColorFG)
+	}
+	colorBG := color.NRGBA{R: 0xF2, G: 0xF2, B: 0xF2, A: 0x0}
+	if item.TextColorBG != colorBG {
+		t.Log(item.TextColorBG)
+		t.Errorf("wrong BG color: %s", item.TextColorBG)
+	}
+}
+func TestLoadMakroCollection2Items(t *testing.T) {
+	path := filepath.Join(pathToTestMakroCollection, testFilesMakroCollection[1])
+	out, err := LoadMakroCollection(path)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	if len(out) != 2 {
+		t.Log(out)
+		t.Error("wrong len")
+		t.FailNow()
+	}
+	item := out[0]
+	if item.Name != "Blenda" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "BlendaKat" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "Blenda.CMK" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+
+	item = out[1]
+	if item.Name != "custom" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "Kategoria" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "custom.CMK" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+}
+func TestLoadMakroCollection2ItemsSameNameMissingFileds(t *testing.T) {
+	path := filepath.Join(pathToTestMakroCollection, testFilesMakroCollection[2])
+	out, err := LoadMakroCollection(path)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	if len(out) != 2 {
+		t.Log(out)
+		t.Error("wrong len")
+		t.FailNow()
+	}
+	item := out[0]
+	if item.Name != "1" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+
+	item = out[1]
+	if item.Name != "1" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+}
+func TestLoadMakroCollectionCompleteExample(t *testing.T) {
+	path := filepath.Join(pathToTestMakroCollection, testFilesMakroCollection[3])
+	out, err := LoadMakroCollection(path)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	if len(out) != 63 {
+		t.Log(out)
+		t.Error("wrong len")
+		t.FailNow()
+	}
+	item := out[27]
+	if item.Name != "Blum Zawiasy Równoległe" {
+		t.Errorf("wrong name: %s", item.Name)
+	}
+	if item.Category != "DRZWI" {
+		t.Errorf("wrong category: %s", item.Category)
+	}
+	if item.FileName != "Blum Zawiasy\\Równoległy.CMK" {
+		t.Errorf("wrong file name: %s", item.FileName)
+	}
+	// Blum Zawiasy Równoległe cat DRZWI
+}
